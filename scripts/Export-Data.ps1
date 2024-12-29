fc528d4ad7d82802f291aecea7dcbc2382cd905c
Lipatant <virgile.berrier@gmail.com>
2024-12-29 18:17:16 +0100

    build: updating Export-Data script to include removing the item first

--- scripts/Export-Data.ps1
+++ scripts/Export-Data.ps1
@@ -1,1 +1,2 @@
+Remove-Item "$env:APPDATA\.minecraft\saves\Lipatant's Artefacts\datapacks\LipatantsArtefactData" -Recurse
 Copy-Item "..\LipatantsArtefactData" "$env:APPDATA\.minecraft\saves\Lipatant's Artefacts\datapacks\" -Recurse
