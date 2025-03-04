3f6a6ad75ff10a6b4721f0210368f3d27cd39b6f
Lipatant <virgile.berrier@gmail.com>
2025-03-04 13:28:38 +0100

    doc: adding rarity and obtention filtering to the wiki

---
+++ wiki/src/item-container.js
@@ -0,0 +1,77 @@
+const itemContainer = document.getElementById("item-container")
+
+function checkForObtention(element, obtention) {
+    if (obtention === "") {
+        return true;
+    }
+    const children = element.children[0].children;
+    let child;
+    for (let i = 0; i < children.length; i++) {
+        child = children[i];
+        if (child.tagName === "LIPARTEFACT-OBTENTION" && child.hasAttribute("id")) {
+            if (Array.isArray(obtention)) {
+                if (obtention.includes(child.getAttribute("id"))) {
+                    return true
+                }
+            } else if (child.getAttribute("id") === obtention) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+function checkForRarity(element, rarity) {
+    if (rarity === "") {
+        return true;
+    }
+    if (element.hasAttribute("rarity")) {
+        return element.getAttribute("rarity") === rarity;
+    }
+    return false
+}
+
+function filterItemContainerByObtention(obtention = "") {
+    let child;
+    const children = itemContainer.children;
+    for (let i = 0; i < children.length; i++) {
+        child = children[i];
+        if (checkForObtention(child, obtention)) {
+            if (child.classList.contains("hide")) {
+                child.classList.remove("hide");
+            }
+        } else {
+            if (!child.classList.contains("hide")) {
+                child.classList.add("hide");
+            }
+        }
+    }
+}
+
+function filterItemContainerByRarity(rarity = "") {
+    let child;
+    const children = itemContainer.children;
+    for (let i = 0; i < children.length; i++) {
+        child = children[i];
+        if (checkForRarity(child, rarity)) {
+            if (child.classList.contains("hide")) {
+                child.classList.remove("hide");
+            }
+        } else {
+            if (!child.classList.contains("hide")) {
+                child.classList.add("hide");
+            }
+        }
+    }
+}
+
+function filterItemContainerReset() {
+    let child;
+    const children = itemContainer.children;
+    for (let i = 0; i < children.length; i++) {
+        child = children[i];
+        if (child.classList.contains("hide")) {
+            child.classList.remove("hide");
+        }
+    }
+}
